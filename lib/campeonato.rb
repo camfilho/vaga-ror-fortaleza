ba1954f2eb56183795f6f077858565a57d879c1d
camfilho <camfilho@ufrn.edu.br>
2020-02-01 19:08:09 -0300

    add campeonato class

--- lib/campeonato.rb
+++ lib/campeonato.rb
@@ -1,4 +1,77 @@
 require_relative './partida.rb'
 
 class Campeonato
+  private
+
+  attr_accessor :todas_partidas, :todos_jogadores
+
+  public
+
+  def pegar_partidas
+    todas_partidas
+  end
+
+  def gerar_partidas(partidas)
+    self.todas_partidas = []
+    partidas.each_slice(3) do |jogos|
+      partida = Partida.new(jogos)
+      partida.pontuar_partida
+      todas_partidas.push partida
+    end
+  end
+
+  def gerar_ponto_por_jogador
+    self.todos_jogadores = Hash.new(Jogador.new(nil))
+    todas_partidas.each do |p|
+      todos_jogadores[p.jogador1.name] = p.jogador1 + todos_jogadores[p.jogador1.name]
+      todos_jogadores[p.jogador2.name] = p.jogador2 + todos_jogadores[p.jogador2.name]
+    end
+    todos_jogadores
+  end
+
+  def classificar
+    todos_jogadores.sort_by do |_name, jg|
+      [-jg.pontos_totais,
+       -jg.num_vitoria_totais,
+       -jg.num_vitoria_fora,
+       -jg.pulos_bonus,
+       jg.punicao]
+    end
+  end
+
+  def gerar_pre_formato(classificado)
+    posicao = 0
+    classificado.map do |name, jg|
+      posicao += 1
+
+      [posicao,
+       name,
+       jg.pontos_totais,
+       jg.num_vitoria_totais,
+       jg.num_vitoria_fora,
+       jg.pulos_bonus,
+       jg.punicao]
+    end
+  end
+
+  def gerar_relatorio
+    data = 'Classificação,'\
+            'Nome,Pontos Totais,Número de Vitórias,'\
+            'Número de Vitórias Fora de Casa,'\
+            'Pulos Bônus,'\
+            "Punições\n"
+    data << gerar_pre_formato(classificar).map { |p| p.join(',') }.join("\n")
+    File.write('resultado.csv', data)
+  end
+
+  def self.gerar_classificacao(partidas)
+    campeonato = Campeonato.new
+    campeonato.gerar_partidas(partidas)
+    campeonato.gerar_ponto_por_jogador
+    if campeonato.gerar_relatorio
+      :ok
+    else
+      :error
+    end
+  end
 end
--- lib/partida.rb
+++ lib/partida.rb
@@ -45,6 +45,7 @@ class Partida
     jogador2
       .pulos_bonus += @pulos_por_partida2
         .reduce(0) { |acc, pulos| pulos > 10 ? acc + 2 : acc }
+
   end
 
   def pulos_iguais?(pulos)
@@ -52,10 +53,10 @@ class Partida
   end
 
   def adicionar_dez_porcento
-    jogador1.pulos_bonus = (jogador1.pulos_totais * 1.1).round if pulos_iguais? @pulos_por_partida1
+    jogador1.pulos_bonus += (jogador1.pulos_totais * 0.1).round if pulos_iguais? @pulos_por_partida1
     return unless pulos_iguais? @pulos_por_partida2
 
-    jogador2.pulos = (jogador2.pulos_totais * 1.1).round
+    jogador2.pulos_bonus += (jogador2.pulos_totais * 0.1).round
   end
 
   def punir?(pulos)
@@ -77,6 +78,7 @@ class Partida
   def winner
     jog1 = jogador1
     jog2 = jogador2
+
     @winner ||= jog1.pulos_totais > jog2.pulos_totais ? jog1 : jog2
   end
 
